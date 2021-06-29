33aa3643be1c929573586887a43bcbecee142162
danielpausch <86592084+danielpausch@users.noreply.github.com>
2021-06-29 13:12:22 +0200

    add console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAB5shared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
